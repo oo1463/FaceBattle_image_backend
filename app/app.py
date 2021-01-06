c3f322e1cf76559070cd8514bebe75c4afad73f1
oo1463 <oooo1463@gmail.com>
2021-01-06 22:21:22 +0900

    update requirement, restapi

--- app/app.py
+++ app/app.py
@@ -1,17 +1,26 @@
-from flask import Flask,render_template
-import socket
-
+from flask import Flask, render_template, jsonify, request, Response
+from flask_restful import Resource, Api
+import numpy as np
+# import tensorflow
+import cv2
+import json
 app = Flask(__name__)
 
 
-@app.route("/")
-def index():
-    try:
-        host_name = socket.gethostname()
-        host_ip = socket.gethostbyname(host_name)
-        return render_template('index.html', hostname=host_name, ip=host_ip)
-    except:
-        return render_template('error.html')
+@app.route("/images", methods=['POST'])
+def predict():
+    req = request
+    # uploaded_files = request.files.getlist("file")
+    # print(req.header)
+
+    nparr = np.fromstring(req.data, np.uint8)
+
+    img = cv2.imdecode(nparr, cv2.IMREAD_COLOR)
+    print(img)
+
+    response = {'message': 'image received. size={}x{}x{}'.format(img.shape[1], img.shape[0], img.shape[2])}
+
+    return jsonify(response)
 
 
 if __name__ == "__main__":
